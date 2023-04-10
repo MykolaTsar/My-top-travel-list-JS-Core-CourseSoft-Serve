4f748752e3e40b8c56e8bce1188e9955af3985b2
Mykola Tsar <Mykola.tsar@gmail.com>
2023-04-10 23:16:21 +0300

    added new changes

--- index.js
+++ index.js
@@ -1,16 +1,67 @@
 const addTravelModal = document.querySelector('#add-modal');
 const startAddTravelButton = document.querySelector('header button');
+const backdropElement = document.querySelector('#backdrop');
+const canselButtonModal = addTravelModal.querySelector('.btn--passive');
+const addButtonModal = canselButtonModal.nextElementSibling;
+const inputs = addTravelModal.querySelectorAll('input'); 
+console.log(inputs);
 
-const joinCanselButtonModal = document.querySelector('.btn--passive');
+const travels = [];
 
+const toggleBackdropElement = () => {
+  backdropElement.classList.toggle('visible');
+}
 
 const toggleTravelModal = () => {
   addTravelModal.classList.toggle('visible');
+  toggleBackdropElement();
 }
 
-const removeTravelModal = () => {
+const clearTravelInputs = () => {
+  for (const userInput of inputs) {
+    userInput.value = '';
+  }
+}
+
+const addTravel = () => {
+  const titleValue = inputs[0].value;
+  const imgValue = inputs[1].value;
+  const ratingValue = inputs[2].value;
+  const dateValue = inputs[3].value;
+
+  if (
+    titleValue.trim() === '' ||
+    imgValue.trim() === '' ||
+    ratingValue.trim() === '' ||
+    +ratingValue < 1 ||
+    +ratingValue > 10 ||
+    isNaN(Date.parse(dateValue))
+    // || !/^\d{4}-\d{2}-\d{2}$/.test(dateValue)//можна використати RegularExp
+  ) {
+    alert('You schould enter correct values!');
+  }
+
+  const newTravel = {
+    title: titleValue,
+    image: imgValue,
+    rating: ratingValue,
+    date: dateValue
+  }
+
+  travels.push(newTravel);
+  console.log(travels);
+  toggleTravelModal();
+  clearTravelInputs();
+}
+
+
+const cancelTravelModal = () => {
   addTravelModal.classList.remove('visible');
+  backdropElement.classList.remove('visible');
+  clearTravelInputs();
 }
 
 startAddTravelButton.addEventListener('click', toggleTravelModal);
-joinCanselButtonModal.addEventListener('click', removeTravelModal);
+canselButtonModal.addEventListener('click', cancelTravelModal);
+backdropElement.addEventListener('click', toggleTravelModal);
+addButtonModal.addEventListener('click', addTravel);
